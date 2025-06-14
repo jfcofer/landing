2cefa89fc45a460f5820b3c1f035a69692ba884e
leozam02 <ltzambra@espol.edu.ec>
2025-06-14 00:34:08 -0500

    Changing Turistic Section

--- assets/js/atracciones.js
+++ assets/js/atracciones.js
@@ -2,7 +2,7 @@ const API_KEY = "5ae2e3f221c38a28845f05b6e14d7215fc0346fe5d7a049649c323b8";
   /*const LAT = -2.0482212839050518; // Latitud del hotel
   const LON = -80.73457306502202; // Longitud del hotel
   const RADIUS = 5000; // ahora: 170 km
--2.2055814118800727, -80.96441417644762*/
+    -2.2055814118800727, -80.96441417644762*/
 
   /*const LAT = -2.2055814118800727; // tu nueva latitud
   const LON = -80.96441417644762; // tu nueva longitud
@@ -36,20 +36,19 @@ fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=${RADIUS}&lon=${L
       contenedor.innerHTML = "<p>No hay atracciones válidas con nombre único.</p>";
       return;
     }
+    
+    contenedor.innerHTML = ""; // Limpiar antes de agregar
 
-    //Mostrar tarjetas
     lugaresFiltrados.forEach(lugar => {
       const tarjeta = document.createElement("div");
-      tarjeta.className = "bg-white shadow-lg rounded-lg p-5 text-left";
-
+      tarjeta.className = "tarjeta-atraccion";
       tarjeta.innerHTML = `
-        <h3 class="text-lg font-bold text-primary mb-1">${lugar.name}</h3>
-        <p class="text-sm text-gray-600">Categoría: <span class="italic">${(lugar.kinds || "").split(",")[0]}</span></p>
+        <h3 class="atraccion-nombre">${lugar.name}</h3>
+        <div class="atraccion-categoria">${(lugar.kinds || "").split(",")[0]}</div>
       `;
-
       contenedor.appendChild(tarjeta);
     });
-  })
+    })
   .catch(error => {
     console.error("Error al obtener atracciones:", error);
     document.getElementById("lugares").innerHTML = "<p>Error al cargar las atracciones.</p>";
